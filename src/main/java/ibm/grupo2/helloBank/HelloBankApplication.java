b8916b7b0ddb3699ff41979664100a5fd3296a93
leo-demetrio <leocdemetrio@gmail.com>
2022-09-17 15:02:17 -0300

    Feat: Config spring security

--- src/main/java/ibm/grupo2/helloBank/HelloBankApplication.java
+++ src/main/java/ibm/grupo2/helloBank/HelloBankApplication.java
@@ -4,8 +4,9 @@ import ibm.grupo2.helloBank.Repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class HelloBankApplication {
 
 	public static void main(String[] args) {
