1ffe341209c8b53dae446520dade437a68467549
leo-demetrio <leocdemetrio@gmail.com>
2022-09-18 08:30:18 -0300

    Fix: Fix algorithm token

--- src/main/java/ibm/grupo2/helloBank/security/JWTValidateFilter.java
+++ src/main/java/ibm/grupo2/helloBank/security/JWTValidateFilter.java
@@ -3,6 +3,7 @@ package ibm.grupo2.helloBank.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
+@Log4j2
 public class JWTValidateFilter extends BasicAuthenticationFilter {
 
     public static final String HEADER_ATRIBUT = "Authorization";
@@ -42,13 +43,13 @@ public class JWTValidateFilter extends BasicAuthenticationFilter {
             return;
         }
         String token = atribut.replace(HEADER_BEARER, "");
-        UsernamePasswordAuthenticationToken authenticationToken = getAuthenticationToken(atribut);
+        UsernamePasswordAuthenticationToken authenticationToken = getAuthenticationToken(token);
 
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         chain.doFilter(request, response);
     }
     private UsernamePasswordAuthenticationToken getAuthenticationToken(String token){
-        String customer = JWT.require(Algorithm.HMAC384(token))
+        String customer = JWT.require(Algorithm.HMAC512(JWTAuthenticateFilter.TOKEN_PASSWORD))
                 .build()
                 .verify(token)
                 .getSubject();
